8437721199deca62c1bf4106d8b2603c2bfc8651
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-10-14 04:37:48 +0100

    init

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => html);
-
-app.Run();
-
 var html = $"""
 <html>
     <head>
         <title>ASP.NET Core</title>
     </head>
     <body>
-        <h1> V1 </h1>
+        <h1> This is V1 </h1>
     </body>
 </html>
 """;
+
+app.MapGet("/", () => html);
+
+app.Run();
+
 
