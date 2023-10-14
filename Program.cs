51adca6126c0362a9d929aec68dc73dce3d0b030
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-10-14 04:21:13 +0100

    init

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "v4");
+app.MapGet("/", () => html);
 
 app.Run();
 
+var html = $"""
+<html>
+    <head>
+        <title>ASP.NET Core</title>
+    </head>
+    <body>
+        <h1> V1 </h1>
+    </body>
+</html>
+"""
 
---
+++ swap.ps1
@@ -0,0 +1,1 @@
+Az webapp deployment slot swap -g cook -n mvpswac1 --slot staging --target-slot production
